def2f7c0f926d00d23189ef8b3c7ee375c85c8c7
Lin Diniz <gabrielvdiniz@gmail.com>
2021-01-02 16:45:28 +0000

    navbar not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import NavbarElement from './componets/Navbar';
 function App() {
   return (
     <div className="App">
+      <NavbarElement />
       <header className="App-header">
       <h1>Teste kk</h1> 
       </header>
